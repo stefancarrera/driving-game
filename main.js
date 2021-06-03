f5074499b75989f0dda19d668f4e983fca3c6978
Stefan Carrera <stefancarrera@gmail.com>
2021-06-03 17:27:46 -0500

    Completed issue-2, user can turn their vehicle.

---
+++ main.js
@@ -0,0 +1,13 @@
+var $ship = document.querySelector('.fa-space-shuttle');
+
+window.addEventListener('keydown', function (event) {
+  if (event.key === 'ArrowRight') {
+    $ship.className = 'fas fa-space-shuttle right';
+  } else if (event.key === 'ArrowLeft') {
+    $ship.className = 'fas fa-space-shuttle left';
+  } else if (event.key === 'ArrowUp') {
+    $ship.className = 'fas fa-space-shuttle up';
+  } else if (event.key === 'ArrowDown') {
+    $ship.className = 'fas fa-space-shuttle down';
+  }
+});
