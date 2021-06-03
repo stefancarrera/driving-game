ab3b4e4d41ed8c79d0822489a148cc2889be6c77
Stefan Carrera <stefancarrera@gmail.com>
2021-06-03 18:19:15 -0500

    Added a spacebar eventlistener.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var $ship = document.querySelector('.fa-space-shuttle');
 
 window.addEventListener('keydown', function (event) {
+
   if (event.key === 'ArrowRight') {
     $ship.className = 'fas fa-space-shuttle right';
   } else if (event.key === 'ArrowLeft') {
@@ -9,5 +10,11 @@ window.addEventListener('keydown', function (event) {
     $ship.className = 'fas fa-space-shuttle up';
   } else if (event.key === 'ArrowDown') {
     $ship.className = 'fas fa-space-shuttle down';
+  }
+
+  if (event.key === ' ') {
+    $ship.animate([{
+      direction: 'normal'
+    }]);
   }
 });
